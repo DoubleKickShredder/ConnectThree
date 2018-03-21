bcb29ad519ffb708cdaea0ecd6c7e8462fb779be
Jeremiah Lewis <jeremiahlewis0710@gmail.com>
2018-03-21 13:13:31 -0400

    Initial commit

---
+++ app/src/main/java/com/jeremiah/connectthree/Player.java
@@ -0,0 +1,19 @@
+package com.jeremiah.connectthree;
+
+import android.view.View;
+
+/**
+ * Created by Jeremiah on 11/30/2017.
+ */
+
+public class Player {
+    String color;
+    int piecesCount;
+
+    public void takeTurn(View view){
+
+
+
+    }
+
+}
